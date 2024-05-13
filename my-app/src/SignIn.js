0f09c9bcf8c860b577300cfc11da6b08277ac8cc
Glory Dounce <glorynsimba@Glorys-MBP.lan>
2024-05-13 13:11:32 -0400

    push

--- my-app/src/SignIn.js
+++ my-app/src/SignIn.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
+import { Container } from "react-bootstrap";
 
-const AUTH_URL = "https://accounts.spotify.com/authorize"
+const AUTH_URL =
+  "https://accounts.spotify.com/authorize? client_id=56457ed506eb4fd7b916be6e7573f55e";
 
 export default function SignIn() {
-    return(
-        
-    )
+  return (
+    <Container className="d-flex justify-s">
+      <a className="btn btn-success btn-lg" href={AUTH_URL}>
+        SignIn with spotify
+      </a>
+    </Container>
+  );
 }
